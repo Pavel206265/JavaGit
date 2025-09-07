e966b0e5dd887c3cd89cfb593d3e95073945cccd
Pavel206265 <pchudakovsky@gmail.com>
2025-09-08 00:38:26 +0200

    Change out

--- src/com/bru/Main.java
+++ src/com/bru/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("bbblhrhrdb");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
 	// write your code here
     }
 }
